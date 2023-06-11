2dcca313cb8d1a3bca35e94c5c407502bfcd9251
qubix00n <kubus.dolecki@gmail.com>
2023-06-11 16:38:47 +0200

    Implemented parsing html documents

--- documentreader.cpp
+++ documentreader.cpp
@@ -4,6 +4,8 @@
 
 #include <fstream>
 #include <stdexcept>
+#include <unordered_map>
+#include <cctype>
 
 namespace hotdocs {
 
@@ -35,7 +37,53 @@ std::string DocumentReader::documentContents(const fs::path &path) {
 }
 
 void DocumentReader::tryParseAsHTML(const std::string &fileContent, std::string &buffer) {
-	info("length: ", fileContent.length());
+	static std::unordered_map<std::string, std::string> escapeSequences = {
+		{ "lt", "<" },
+		{ "gt", ">" },
+		{ "amp", "&" },
+	};
+
+	size_t n = fileContent.length();
+	size_t i;
+
+	auto isFollowedBySubstring = [&fileContent, &i](const std::string &str) -> bool {
+		return std::strncmp(&fileContent[i + 1], str.c_str(), str.length()) == 0;
+	};
+
+	for(i = 0; i < n; ++i) {
+		char c = fileContent[i];
+		if(std::isprint(c)) {
+			switch(c) {
+				case '<': {
+					if(isFollowedBySubstring("script")) {
+						while(i < n && !isFollowedBySubstring("</script>")) i++;
+					} else if(isFollowedBySubstring("style")) {
+						while(i < n && !isFollowedBySubstring("</style>")) i++;
+					} else {
+						while(i < n && c != '>') c = fileContent[++i];
+					}
+					buffer += ' ';
+				} break;
+				case '&': {
+					size_t j = i; std::string escape;
+					while(j < n && c != ';' && j - i < 10) {
+						escape += (c = fileContent[++j]);
+					}
+					escape.pop_back(); // remove the semicolon
+					
+					if(escapeSequences.find(escape) != escapeSequences.end()) {
+						buffer += escapeSequences.at(escape);
+						i += (escape.length() + 1);
+					} else {
+						throw std::runtime_error("unknown escape sequence: &"s + escape + ";"s);
+					}
+				} break;
+				default: {
+					buffer += c;
+				}
+			}
+		}
+	}
 }
 
 } // namespace hotdocs
